97a77f94e31d7a4df73694042d984cc24add91db
Adrian Felipe Guerra <adrianguerra9703@gmail.com>
2026-09-09 22:23:04 -0500

    refactor(campaigns): export application services

--- backend/app/modules/campaigns/application/__init__.py
+++ backend/app/modules/campaigns/application/__init__.py
@@ -1,3 +1,11 @@
+from app.modules.campaigns.application.angles import CampaignAngleService
+from app.modules.campaigns.application.landings import CampaignLandingService
+from app.modules.campaigns.application.offers import CampaignOfferService
 from app.modules.campaigns.application.service import CampaignService
 
-__all__ = ["CampaignService"]
+__all__ = [
+    "CampaignService",
+    "CampaignAngleService",
+    "CampaignOfferService",
+    "CampaignLandingService",
+]
